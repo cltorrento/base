dd1113f8de789a93760c916ac848837dea748151
Carlos Torrento <cltorrento@gmail.com>
2018-08-19 16:52:59 +0200

    Base Clean

---
+++ src/main/java/com/erp/comercial/ComercialApplication.java
@@ -0,0 +1,12 @@
+package com.erp.comercial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComercialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComercialApplication.class, args);
+	}
+}
